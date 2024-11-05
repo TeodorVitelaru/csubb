d34b618076e1a342b1fcd9dd7add0c6dbd35d5f2
unknown <teodor_vitelaru@yahoo.com>
2024-11-05 14:56:40 +0200

    Removed unnecessary files

--- python projects/Lab9/utils/file_utils.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-
-def copy_file_content(source_file, destination_file):
-    shutil.copyfile(source_file, destination_file)
-
-def clear_file_content(filename):
-    with open(filename, mode="w") as f:
-        pass
--- python projects/PracticFP/utils/file_utils.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-
-def clear_file_content(filename):
-    with open(filename, mode="w") as f:
-        pass
-
-def copy_file_content(source_file, destination_file):
-    shutil.copyfile(source_file, destination_file)
